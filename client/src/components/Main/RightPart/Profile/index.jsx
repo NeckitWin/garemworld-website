c1a2eb9ee593db34f6f1c2d6d5a967e70a083574
NeckitWin <neckit.v@gmail.com>
2024-04-03 22:31:30 +0200

    слегка переделал стиль кнопок

---
+++ client/src/components/Main/RightPart/Profile/index.jsx
@@ -0,0 +1,11 @@
+import s from './Profile.module.css'
+
+const Profile = () => {
+    return (
+        <div className={s.profile}>
+            <div><img src="" alt=""/></div>
+        </div>
+    )
+}
+
+export default Profile
--- client/src/components/Main/RightPart/index.jsx
+++ client/src/components/Main/RightPart/index.jsx
@@ -3,11 +3,13 @@ import InfoFrame from "./InfoFrame";
 import Monitoring from "./Monitoring";
 import Votes from "./Votes";
 import Login from "./Login";
+import Profile from "./Profile";
 
 const RightPart = () => {
     return (
         <div className={s.right_part}>
             <Login />
+            <Profile />
             <InfoFrame/>
             <Votes/>
             <Monitoring/>
