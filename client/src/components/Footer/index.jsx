ba3971bbac23cef02daeae3029a880d786d499dc
NeckitWin <neckit.v@gmail.com>
2024-04-26 01:21:58 +0200

    добавил футер

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,9 +1,66 @@
 import s from './Footer.module.css'
+import {NavLink} from "react-router-dom";
 
 const Footer = () => {
+    const mainlinks = [
+        {
+            content: 'Как начать играть?',
+            href: '/start'
+        },
+        {
+            content: 'Скачать лаунчер Windows',
+            href: 'https://launcher.garemworld.su/GaremWorld.exe'
+        },
+        {
+            content: 'Скачать лаунчер Linux',
+            href: 'https://launcher.garemworld.su/GaremWorld.jar'
+        },
+        {
+            content: 'Команда проекта',
+            href: '/info/squad'
+        }
+    ]
+    const sociallinks = [
+        {
+            content: 'Discord',
+            href: 'https://discord.gg/tDGak75V4u'
+        },
+        {
+            content: 'Техническая поддержка',
+            href: 'https://discord.gg/tDGak75V4u'
+        },{
+            content: 'Публичная оферта',
+            href: '/offer'
+        },
+        {
+            content: 'Политика конфиденциальности',
+            href: '/privacy'
+        }
+    ]
     return (
         <footer className={s.footer}>
-
+            <div>
+                <NavLink to="/"><img src="/img/full-logo.png" alt="logo"/></NavLink>
+                <nav>
+                    <ul>
+                        {
+                            mainlinks.map((el, index) => (
+                                <li key={index}><NavLink className={s.links} to={el.href}>{el.content}</NavLink></li>
+                            ))}
+                    </ul>
+                    <ul>
+                        {
+                            sociallinks.map((el, index) => (
+                                <li key={index}><a className={s.links} target="_blank" rel="noreferrer" href={el.href}>{el.content}</a></li>
+                            ))}
+                    </ul>
+                    <ul>
+                        <li><a target="_blank" rel="noreferrer" href="https://neckitwin.github.io">Владелец: NeckitWin</a></li>
+                        <li>© 2024 GaremWorld</li>
+                        <li>Все права защищены</li>
+                    </ul>
+                </nav>
+            </div>
         </footer>
     )
 }
--- client/src/components/Main/LeftPart/index.jsx
+++ client/src/components/Main/LeftPart/index.jsx
@@ -9,10 +9,11 @@ const LeftPart = () => {
     useEffect(() => {
         if (location.pathname === '/') setBanner('https://64.media.tumblr.com/5eb49aff7c0608daaed49bbf03bbe2bf/d04e4c3932c2092e-68/s500x750/8b63afe406f306a22f18106a7fbc76d86475750a.gifv')
         else if (location.pathname === '/start') setBanner('https://media4.giphy.com/media/WoRFcCl7cINtZcz7dC/200.gif')
-        else if (location.pathname === '/servers') setBanner('https://i.pinimg.com/originals/55/6c/e1/556ce1dc9f164dfe0723942e36c74e9f.gif')
+        else if (location.pathname.startsWith('/servers')) setBanner('https://i.pinimg.com/originals/55/6c/e1/556ce1dc9f164dfe0723942e36c74e9f.gif')
         else if (location.pathname === '/donate') setBanner('https://i.pinimg.com/originals/6e/85/f7/6e85f7e0111ac569249afb790efff78f.gif')
-        else if (location.pathname === '/rules') setBanner('https://i.pinimg.com/originals/8e/d6/8c/8ed68cb0e0257aef072b8431648b4e81.gif')
-        else if (location.pathname === '/') setBanner('')
+        else if (location.pathname.startsWith('/rules')) setBanner('https://i.pinimg.com/originals/8e/d6/8c/8ed68cb0e0257aef072b8431648b4e81.gif')
+        else if (location.pathname.startsWith('/info')) setBanner('https://i.pinimg.com/originals/75/9e/0d/759e0d7653c18b3d90dd0ab334b75ba9.gif')
+        else if (location.pathname === '/user') setBanner('https://i.redd.it/kvxwushpheu71.gif')
         else setBanner('https://64.media.tumblr.com/5eb49aff7c0608daaed49bbf03bbe2bf/d04e4c3932c2092e-68/s500x750/8b63afe406f306a22f18106a7fbc76d86475750a.gifv')
 
     }, [location.pathname]);
